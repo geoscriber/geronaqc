672d90ddcdf7386f5664446c93321e57cee310e2
krquinton <nscrexgst.dotr@gmail.com>
2023-03-11 22:15:22 +0800

    x
    xxx

---
+++ data.js
@@ -0,0 +1,156 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-top-view",
+      "name": "Top View",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5902957980646848,
+        "pitch": 1.5707963267948966,
+        "fov": 1.4333212056084728
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.2157302574975155,
+          "pitch": 1.3491319426798007,
+          "title": "Subject Property",
+          "text": "Topview"
+        }
+      ]
+    },
+    {
+      "id": "1-top-view-2",
+      "name": "Top View 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.2424368573937805,
+        "pitch": 1.1031839813029407,
+        "fov": 1.4333212056084728
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-ground",
+      "name": "Ground",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.738653535151916,
+        "pitch": 0.10548504437591255,
+        "fov": 1.4333212056084728
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.4404504673639646,
+          "pitch": -0.19391371645511768,
+          "title": "Subject Property",
+          "text": "Groundview"
+        }
+      ]
+    },
+    {
+      "id": "3-back",
+      "name": "Back",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.12961473234031118,
+          "pitch": 1.1489925367024298,
+          "title": "Subject Property",
+          "text": "Backview of the subject property"
+        }
+      ]
+    }
+  ],
+  "name": "Gerona's Property",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
